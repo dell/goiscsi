e4115d445c3dfcf43ae1e0547ce8e7516cb6c48c
Harish P <harish_p4@dell.com>
2022-06-02 14:55:41 +0530

    Fixed IP validation

--- utils.go
+++ utils.go
@@ -2,15 +2,28 @@ package goiscsi
 
 import (
 	"errors"
+	"net"
 	"regexp"
 )
 
 func validateIPAddress(ip string) error {
-	// Regex to validate IPV4 Ex: 10.0.0.0:1111
+	isValidIP := true
+	isValidPortal := true
+
+	// validtes only IP
+	if net.ParseIP(ip) == nil {
+		isValidIP = false
+	}
+
+	// Regex to validate IPV4 with port - for portal validation Ex: 10.0.0.0:1111
 	const exp = `^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]):[0-9]+$`
 	r := regexp.MustCompile(exp)
 	if !r.MatchString(ip) {
-		return errors.New("Error invalid IP address")
+		isValidPortal = false
+	}
+	// Either valid IP/portal address should be given
+	if !isValidIP && !isValidPortal {
+		return errors.New("Error invalid IP or portal address")
 	}
 	return nil
 }
